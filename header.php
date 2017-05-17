6efe10fa8f85ddcce747f23004bc5157cf4d1974
solene <solenantoine@gmail.com>
2017-05-17 17:14:34 +0200

    add style

--- header.php
+++ header.php
@@ -5,6 +5,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="<?php echo get_stylesheet_uri()?>">
 <link href="https://fonts.googleapis.com/css?family=Gochi+Hand" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet"> 
 
 </head>
 
@@ -17,14 +18,25 @@
         $categories = array( 'category_name' => 'details-de-la-personne');
             $posts_array = get_posts( $categories );
                 foreach ($posts_array as $post) : setup_postdata( $post );
-                the_category('<h1>','</h1>');?>
-            	<div>
-                    <?php the_content('<p>','</p>'); ?>
-            	</div>
+                    $dontrepeat = $categories->cat_ID;
+                    the_category('<h1>','</h1>');?>
+                	<div>
+                        <?php the_content('<p>','</p>'); ?>
+                	</div>
             <?php endforeach;
+
+            $cat = get_category_by_slug('details-de-la-personne');
+            $categories = get_categories( array( 'orderby' => 'name'));?>
+            <div class="menu">
+                <?php
+                foreach ($categories as $category):
+                    if ( $category->cat_ID != $cat->term_id ) :?>
+                            <?php echo '<a href=#'.$category->name.'><p>'.$category->name.'</p></a>';
+                    endif;
+                endforeach;
         ?>
 
-
+            </div>
 	</header><!-- #masthead -->
 
 	<div id="content" class="site-content">
--- index.php
+++ index.php
@@ -2,25 +2,38 @@
 
 
 <?php global $post;
+$cat = get_category_by_slug('details-de-la-personne');
 $categories = get_categories( array( 'orderby' => 'name'));
-foreach ($categories as $category): ?>
-    <div class="domaine">
-        <?php echo '<h2>'.$category->name.'</h2>';
+    foreach ($categories as $category):
+        if ( $category->cat_ID != $cat->term_id ) :?>
+            <div class="domaine">
+                <?php echo '<h2 id='.$category->name.'>'.$category->name.'</h2>';
 
-        $args = array(
-        	'posts_per_page'   => 5,
-        	'category'    => $category->cat_ID
-        );
-        $posts_array = get_posts( $args );
-        ?>
-        <div>
-            <?php foreach ($posts_array as $post) : setup_postdata( $post ); ?>
-
-        		<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-                <?php the_meta('<p class="periode">','</p>'); ?>
-                <?php the_content('<p>','</p>') ?>
-            <?php endforeach;?>
-        </div>
-    </div>
-    <?php
-endforeach;?>
+                $args = array(
+                	'posts_per_page'   => 5,
+                	'category'         => $category->cat_ID,
+                    'order'            => 'DESC',
+                    'meta_key'         => 'Période',
+                    'orderby'          => 'meta_value_num',
+                );
+                $posts_array = get_posts( $args );
+                ?>
+                <div>
+                    <?php
+                    foreach ($posts_array as $post) : setup_postdata( $post );
+                        ?>
+                		<a class="title" href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+                        <?php
+                        $custom_fields = get_post_custom(0);
+                        $my_custom_field = $custom_fields['Période'];
+                        foreach ( $my_custom_field as $key => $value ) {
+                            echo '<p>'. $value . '</p>';
+                        }
+                        the_content('<p>','</p>');
+                    endforeach;?>
+                </div>
+            </div>
+            <?php
+        endif;
+    endforeach;
+ ?>
